14dbaeb57be9b816487cb2cdce3e0d1ce35f156e
Shlomo Alexander Perlov <perlov3301@gmail.com>
2022-09-04 19:59:07 +0300

    the same as refactoring to use viewBinding

--- app/src/main/java/com/example/thirdemptygithub/MainActivity.kt
+++ app/src/main/java/com/example/thirdemptygithub/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.thirdemptygithub
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.thirdemptygithub.databinding.ActivityMainBinding
+import com.example.thirdemptygithub.utilities.MyClass1
+import com.example.thirdemptygithub.utilities.MyClass2
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val myClass1 = MyClass1()
+        val myClass2 = MyClass2()
+        myClass1.saySomething()
+        myClass2.saySomething()
+
     }
 }
---
+++ app/src/main/java/com/example/thirdemptygithub/utilities/MyClass2.kt
@@ -0,0 +1,11 @@
+package com.example.thirdemptygithub.utilities
+
+import android.util.Log
+
+const val TAG2 = "my_tag"
+
+class MyClass2 {
+    fun saySomething() {
+        Log.d(TAG2, "saySomthing: Hello World from MyClass2")
+    }
+}
