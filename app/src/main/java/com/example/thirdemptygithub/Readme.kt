215e8e2b03e241a43f6f676c1b505e16c1025044
Shlomo Alexander Perlov <perlov3301@gmail.com>
2022-09-08 22:35:17 +0300

    firefox new token

--- app/src/main/java/com/example/thirdemptygithub/Readme.kt
+++ app/src/main/java/com/example/thirdemptygithub/Readme.kt
@@ -3,4 +3,5 @@ package com.example.thirdemptygithub
 class Readme {
     // The URL to enter has this format:
 // https://{TOKEN}@github.com/{USER}/{REPO}.git
+    // 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
 }
